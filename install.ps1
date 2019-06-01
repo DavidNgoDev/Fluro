411cbde0f2e3e2d263c1b8c3423039fcc11da298
DavidNgo_Dev <13045581+DavidNgoDev@users.noreply.github.com>
2019-06-01 16:50:52 +1000

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,327 @@
+﻿##############################################################
+# Fluro Install Script                                       #
+##############################################################
+# Windows 10 Custom Installation Script, Using Powershell    #
+##############################################################
+# Instructions #
+#############################################  
+# 1) Run PowerShell as administrator        #
+# 2) Type Set-ExecutionPolicy Unrestricted  # 
+# 3) Run Fluro by typing ./install.ps1      #
+#############################################
+param (
+  [string]$password = "",
+  [bool]$nochecks = $false
+)
+function installBoxStarter()
+{
+  <#
+  .SYNOPSIS
+  Install BoxStarter on the current system  
+  .DESCRIPTION
+  Install BoxStarter on the current system. Returns $true or $false to indicate success or failure. On
+  fresh windows 7 systems, some root certificates are not installed and updated properly. Therefore,
+  this funciton also temporarily trust all certificates before installing BoxStarter.  
+  #>  
+  # https://stackoverflow.com/questions/11696944/powershell-v3-invoke-webrequest-https-error
+  # Allows current PowerShell session to trust all certificates
+  # Also a good find: https://www.briantist.com/errors/could-not-establish-trust-relationship-for-the-ssltls-secure-channel/
+  try {
+  Add-Type @"
+  using System.Net;
+  using System.Security.Cryptography.X509Certificates;
+  public class TrustAllCertsPolicy : ICertificatePolicy {
+  	public bool CheckValidationResult(
+  		ServicePoint srvPoint, X509Certificate certificate,
+  		WebRequest request, int certificateProblem) {
+  		return true;
+  	}
+  }
+"@
+  } catch {
+    Write-Debug "Failed to add new type"
+  }  
+  try {
+  	$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+  } catch {
+  	Write-Debug "Failed to find SSL type...1"
+  }  
+  try {
+  	$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls'
+  } catch {
+  	Write-Debug "Failed to find SSL type...2"
+  }  
+  $prevSecProtocol = [System.Net.ServicePointManager]::SecurityProtocol
+  $prevCertPolicy = [System.Net.ServicePointManager]::CertificatePolicy  
+  Write-Host "[ * ] Installing Boxstarter"
+  # Become overly trusting
+  [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+  [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy  
+  # download and instal boxstarter
+  iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); get-boxstarter -Force  
+  # Restore previous trust settings for this PowerShell session
+  # Note: SSL certs trusted from installing BoxStarter above will be trusted for the remaining PS session
+  [System.Net.ServicePointManager]::SecurityProtocol = $prevSecProtocol
+  [System.Net.ServicePointManager]::CertificatePolicy = $prevCertPolicy
+  return $true
+}
+
+# Check to make sure script is run as administrator
+Write-Host "[+] Checking if script is running as administrator.."
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+if (-Not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+  Write-Host "`t[ERR] Please run this script as administrator`n" -ForegroundColor Red
+  Read-Host  "Press any key to continue"
+  exit
+}
+else {
+  Start-Sleep -Milliseconds 500
+  Write-Host "`tphenomenal " -ForegroundColor Magenta -NoNewLine
+  Start-Sleep -Milliseconds 500
+  Write-Host "cosmic " -ForegroundColor Cyan -NoNewLine
+  Start-Sleep -Milliseconds 500
+  Write-Host "powers " -ForegroundColor Green
+  Start-Sleep -Milliseconds 500
+}
+
+if ($nochecks -eq $false) {
+  
+  # Check to make sure host is supported
+  Write-Host "[+] Checking to make sure Operating System is compatible"
+  if (-Not (((Get-WmiObject -class Win32_OperatingSystem).Version -eq "6.1.7601") -or ([System.Environment]::OSVersion.Version.Major -eq 10))){
+    Write-Host "`t[ERR] $((Get-WmiObject -class Win32_OperatingSystem).Caption) is not supported, please use Windows 7 Service Pack 1 or Windows 10" -ForegroundColor Red
+    exit 
+  }
+  else
+  {
+    Write-Host "`t$((Get-WmiObject -class Win32_OperatingSystem).Caption) supported" -ForegroundColor Green
+  }
+
+  # Check to make sure host has been updated
+  Write-Host "[+] Checking if host has been configured with updates"
+  if (-Not (get-hotfix | where { (Get-Date($_.InstalledOn)) -gt (get-date).adddays(-30) })) {
+    Write-Host "`t[ERR] This machine has not been updated in the last 30 days, please run Windows Updates to continue`n" -ForegroundColor Red
+    Read-Host  "Press any key to continue"
+    exit
+  }
+  else
+  {
+	  Write-Host "`tupdates appear to be in order" -ForegroundColor Green
+  }
+
+  #Check to make sure host has enough disk space
+  Write-Host "[+] Checking if host has enough disk space"
+  $disk = Get-PSDrive C
+  Start-Sleep -Seconds 1
+  if (-Not (($disk.used + $disk.free)/1GB -gt 58.8)){
+    Write-Host "`t[ERR] This install requires a minimum 60 GB hard drive, please increase hard drive space to continue`n" -ForegroundColor Red
+    Read-Host "Press any key to continue"
+    exit
+  }
+  else
+  {
+    Write-Host "`t> 60 GB hard drive. looks good" -ForegroundColor Green
+}
+
+# Get user credentials for autologin during reboots
+Write-Host "[ * ] Getting user credentials ..."
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
+
+if ([string]::IsNullOrEmpty($password)) {
+	$cred=Get-Credential $env:username
+} else {
+	$spasswd=ConvertTo-SecureString -String $password -AsPlainText -Force
+	$cred=New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $env:username, $spasswd
+}
+# TO DO - Verify credentials before continuing
+
+# Install Boxstarter
+Write-Host "[ * ] Installing Boxstarter"
+try {
+  iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); get-boxstarter -Force
+} catch {
+  $rc = installBoxStarter
+  if (-Not $rc) {
+  	Write-Host "[ERR] Failed to install BoxStarter"
+  	Read-Host  "      Press ANY key to continue..."
+  	exit
+  }
+}
+
+# Boxstarter options
+$Boxstarter.RebootOk = $true    # Allow reboots?
+$Boxstarter.NoPassword = $false # Is this a machine with no login password?
+$Boxstarter.AutoLogin = $true # Save my password securely and auto-login after a reboot
+}
+
+#---- TEMPORARY ---
+Disable-UAC
+
+
+#--- Windows Desktop Experience Settings  ---
+
+Disable-GameBarTips
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+
+# Privacy: Let apps use my advertising ID: Disable
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+    New-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo | Out-Null
+}
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo -Name Enabled -Type DWord -Value 0
+
+# WiFi Sense: HotSpot Sharing: Disable
+If (-Not (Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+    New-Item -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting | Out-Null
+}
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting -Name value -Type DWord -Value 0
+
+# WiFi Sense: Shared HotSpot Auto-Connect: Disable
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots -Name value -Type DWord -Value 0
+
+# Start Menu: Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+# To Restore (Enabled):
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 1
+
+# Disable Telemetry (requires a reboot to take effect)
+# Note this may break Insider builds for your organization
+# Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection -Name AllowTelemetry -Type DWord -Value 0
+# Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
+
+# Change Explorer home screen back to "This PC"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 1
+# Change it back to "Quick Access" (Windows 10 default)
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 2
+
+# Better File Explorer
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+# Disable Xbox Gamebar
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0
+
+# Turn off People in Taskbar
+If (-Not (Test-Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
+    New-Item -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People | Out-Null
+}
+Set-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name PeopleBand -Type DWord -Value 0
+
+#--- Windows Features ---
+choco install Microsoft-Hyper-V-All -source windowsFeatures
+choco install Containers -source windowsFeatures
+choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+#--- Tools ---
+choco install docker-for-windows -dvy
+Install-Module -Name posh-docker -Force
+choco install powershell-core -dvy
+choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"' -dvy
+choco install hyper -dvy
+choco install mRemoteNG -dvy
+
+#--- Removing Shit ---
+
+# Feedback Hub
+Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+ 
+# Get Started
+Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+
+# Messaging
+Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
+
+## Candy Crush
+Get-AppxPackage king.com.CandyCrush* | Remove-AppxPackage
+
+# Install Custom Software
+Write-Host "[ * ] Installing Software"
+choco install adobereader -dvy
+choco install googlechrome -dvy
+choco install jre8 -dvy
+choco install firefox -dvy
+choco install notepadplusplus.install -dvy
+choco install 7zip.install -dvy
+choco install vlc -dvy
+choco install winrar -dvy
+choco install git.install -dvy
+choco install putty.install -dvy
+choco install javaruntime -dvy
+choco install skype -dvy
+choco install ccleaner -dvy
+choco install filezilla -dvy
+choco install malwarebytes -dvy
+choco install atom -dvy
+choco install gimp -dvy
+choco install itunes -dvy
+choco install winscp.install -dvy
+choco install dropbox -dvy
+choco install virtualbox -dvy
+choco install wireshark -dvy
+choco install sublimetext3 -dvy
+choco install audacity -dvy
+choco install spotify -dvy
+choco install steam -dvy
+choco install googledrive -dvy
+choco install dolphin -dvy
+choco install mysql.workbench -dvy
+choco install qbittorrent -dvy
+choco install brackets -dvy
+choco install rufus -dvy
+choco install poweriso -dvy
+choco install cpu-z.install -dvy
+choco install nmap -dvy
+choco install greenshot -dvy
+choco install androidstudio -dvy
+choco install adwcleaner -dvy
+choco install arduino -dvy
+choco install yumi -dvy
+choco install handbrake -dvy
+choco install discord -dvy
+choco install kodi -dvy
+choco install f.lux.install -dvy
+choco install obs-studio -dvy
+choco install gitkraken -dvy
+choco install visualstudiocode -dvy
+choco install git -dvy
+choco install nodejs -dvy
+choco install hyper -dvy
+choco install vmwareworkstation -dvy
+choco install windirstat -dvy
+choco install visualstudio2019professional -dvy
+choco install intellijidea-community -dvy
+choco install resharper-platform -dvy
+choco install resharper -dvy
+choco install dotpeek -dvy
+choco install intellijidea-ultimate -dvy
+choco install dotcover -dvy
+choco install dottrace -dvy
+choco install webstorm -dvy
+choco install phpstorm -dvy
+choco install datagrip -dvy
+choco install dotmemory -dvy
+choco install jetbrainstoolbox -dvy
+choco install teamcityaddin -dvy
+choco install goland -dvy
+choco install pycharm -dvy
+choco install rubymine -dvy
+choco install goland -dvy
+choco install adobe-creative-cloud -dvy
+choco install office365business -dvy
+
+# Pinning Things
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles\Google\Chrome\Application\chrome.exe"
+
+#--- Restore Temporary Settings ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
+
+#--- Rename the Computer ---
+# Requires restart, or add the -Restart flag
+$computername = "Fluro Windows Build"
+if ($env:computername -ne $computername) {
+Rename-Computer -NewName $computername -Restart
+if (Test-PendingReboot) { Invoke-Reboot }
